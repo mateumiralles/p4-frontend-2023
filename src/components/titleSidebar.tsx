bd63180e6206e58510b82bfbd51dba313c99f7ce
MateuMiralles <mateumiralles714@gmail.com>
2023-06-22 23:20:02 +0200

    - Country screen basic layout

--- src/components/titleSidebar.tsx
+++ src/components/titleSidebar.tsx
@@ -6,7 +6,7 @@ function TitleSidebar() {
   return (
     <>
        <div className="h-screen sticky top-0  flex items-center flex-column bg-cyan-600">
-          <h1 className="font-semibold m-20 text-7xl text-center     text-gray-100">
+          <h1 className="font-semibold mx-20 text-7xl text-center     text-gray-100">
             COUNTRIES LIST
           </h1>
         </div>
--- src/screens/CountryScreen.tsx
+++ src/screens/CountryScreen.tsx
@@ -5,15 +5,52 @@ function CountryScreen() {
 
   const c = location.state.item;
 
- 
   return (
     <>
       <Link to="/">
         <h2>GO BACK TO THE LIST</h2>
       </Link>
-      <h1>Detail Screen of {c["name"]["common"]}</h1>
-      <img height="100" src={c["flags"]["svg"]} alt={c["flags"]["alt"]} />
-     </>
+      <div className="h-max w-screen flex flex-col">
+        <div className="flex flex-row">
+          <div className="flex flex-col w-1/4 bg-green-300">
+            <h1>{c["name"]["common"]}</h1>
+            <h3>
+              {c["region"]}, {c["subregion"]}
+            </h3>
+            <h3>
+              {c["capital"]}, LAT:{c["latlng"][0]} LANG:{c["latlng"][1]}
+            </h3>
+          </div>
+          <div className="flex flex-col w-3/4 bg-yellow-300">
+            <img
+              className="h-48"
+              src={c["flags"]["svg"]}
+              alt={c["flags"]["alt"]}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center bg-purple-500">
+          <h2>AREA:</h2>
+          <p>{c["area"]}</p>
+        </div>
+        <div className="flex flex-col items-center bg-cyan-500">
+          <h2>POPULATION:</h2>
+          <p>{c["population"]}</p>
+        </div>
+        <div className="flex flex-col items-center bg-stone-400">
+          <h2>LANGUAGE:</h2>
+          {Object.values(c["languages"])}
+        </div>
+        <div className="flex flex-col items-center bg-red-300">
+          <h2>TIMEZONES:</h2>
+          {c["timezones"].map((timezone: string) => {
+            return <p>{timezone}</p>;
+          })}
+          
+        </div>
+        
+      </div>
+    </>
   );
 }
 
