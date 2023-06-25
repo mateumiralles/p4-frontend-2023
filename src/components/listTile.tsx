5f4b537fc90cff2f361a30488be961b42f64c08d
MateuMiralles <mateumiralles714@gmail.com>
2023-06-25 17:21:41 +0200

    ListTile link correction

--- src/components/listTile.tsx
+++ src/components/listTile.tsx
@@ -3,30 +3,36 @@ import { Link } from "react-router-dom";
 const ListTile = (props: any) => {
   return (
     <>
-      <Link className="my-2 flex min-h-min w-full justify-center" state={{ item: props.c }} to={ props.c["name"]["common"]}>
+      <Link
+        className="my-2 flex  w-2/3 justify-center "
+        state={{ item: props.c }}
+        to={props.c["name"]["common"]}
+      >
         <div
-          className="flex h-16  w-2/3  items-center justify-center bg-cyan-600 rounded-lg shadow-cyan-700 shadow-md hover:bg-cyan-700"
-          key={ props.c["name"]["common"]}
+          className="flex h-16  w-full items-center justify-center bg-cyan-600 rounded-lg shadow-cyan-700 shadow-md hover:bg-cyan-700"
+          key={props.c["name"]["common"]}
         >
-            <img
-              className="h-6 mr-3 border border-black"
-              src={ props.c["flags"]["svg"]}
-              alt={ props.c["flags"]["alt"]}
-            />
+          <img
+            className="h-6 mr-3 border border-black"
+            src={props.c["flags"]["svg"]}
+            alt={props.c["flags"]["alt"]}
+          />
           <div>
-            <p className="text-2xl text-white font-bold pb-2">{ props.c["name"]["common"]}</p>
+            <p className="text-2xl text-white font-bold pb-2">
+              {props.c["name"]["common"]}
+            </p>
           </div>
           <div>
             <img
               className="h-6 ml-3 border border-black"
-              src={ props.c["flags"]["svg"]}
-              alt={ props.c["flags"]["alt"]}
+              src={props.c["flags"]["svg"]}
+              alt={props.c["flags"]["alt"]}
             />
           </div>
         </div>
       </Link>
     </>
   );
-}
+};
 
 export default ListTile;
