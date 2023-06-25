14b8cc39fe61237e487577e368688f117d6c72e2
MateuMiralles <mateumiralles714@gmail.com>
2023-06-25 17:23:00 +0200

    searchBar correction

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar = ({ onQuery }: any) => {
   return (
     <>
       <input
-        className="w-1/2 h-16 my-5 rounded-xl p-6 text-center text-3xl font-bold outline-none bg-cyan-400 placeholder-white placeholder-opacity-80"
+        className="w-1/2 h-16 my-5 rounded-xl p-6 text-center text-2xl font-bold outline-none bg-cyan-400 placeholder-white placeholder-opacity-80"
         placeholder="Type here to search for a country!"
         onInput={handleInput}
       >
